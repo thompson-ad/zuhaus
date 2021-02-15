94a7dcd25b17d015b62ce7e26521e716366d8373
Aaron Thompson <thompson_ad@outlook.com>
2021-02-15 09:22:46 +0000

    feat: add auth and connect to DB

--- web/pages/_app.tsx
+++ web/pages/_app.tsx
@@ -1,10 +1,18 @@
+import 'reflect-metadata'
+import React from 'react'
+import { Provider } from 'next-auth/client'
 import '../styles/reset.css'
 import '../styles/theme.css'
 import '../styles/global.css'
 import '../styles/home.css'
+import '../styles/signin.css'
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <Provider session={pageProps.session}>
+      <Component {...pageProps} />
+    </Provider>
+  )
 }
 
 export default MyApp
